70a3c6daf46f75743fdd3b3ecbc5f3005e16db28
Snowbeast138 <snowbeast138@gmail.com>
2024-06-17 19:18:55 -0600

    Navigation

--- EnegApp/src/components/NavBar.tsx
+++ EnegApp/src/components/NavBar.tsx
@@ -1,47 +1,63 @@
 import { Menubar } from "primereact/menubar";
+import { MenuItem } from "primereact/menuitem";
 import Logo from "../assets/Logo_Eneg.jpg";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  //   const navigate = useNavigate();
-  const items = [
+  const navigate = useNavigate();
+  const items: MenuItem[] = [
     {
       label: "Introducción",
       icon: "pi pi-home",
+      command: () => navigate("/Introduccion"),
     },
     {
       label: "Problematización",
       icon: "pi pi-exclamation-triangle",
+      command: () => navigate("/Problematizacion"),
     },
     {
       label: "Planteamiento del problema",
       icon: "pi pi-sitemap",
+      command: () => navigate("/Planteamiento"),
     },
     {
       label: "Justificación",
       icon: "pi pi-comments",
+      command: () => navigate("/Justificacion"),
     },
     {
       label: "Objetivos",
       icon: "pi pi-list",
+      command: () => navigate("/Objetivos"),
     },
     {
       label: "Hipótesis",
       icon: "pi pi-check",
+      command: () => navigate("/Hipotesis"),
     },
     {
       label: "Propuesta de Mejora",
       icon: "pi pi-user-plus",
+      command: () => navigate("/Propuesta"),
     },
     {
       label: "Galería",
       icon: "pi pi-images",
+      command: () => navigate("/Galeria"),
     },
   ];
 
   const start = (
     <>
-      <img src={Logo} alt="Logo" className="cursor-pointer w-16" />
+      <img
+        src={Logo}
+        alt="Logo"
+        className="cursor-pointer w-16"
+        onClick={() => {
+          navigate("/");
+        }}
+      />
     </>
   );
 
--- EnegApp/src/main.tsx
+++ EnegApp/src/main.tsx
@@ -6,9 +6,41 @@ import "./index.css";
 import "primereact/resources/themes/tailwind-light/theme.css"; /* puedes cambiar el tema */
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Introduccion",
+  },
+  {
+    path: "/Problematizacion",
+  },
+  {
+    path: "/Planteamiento",
+  },
+  {
+    path: "/Justificacion",
+  },
+  {
+    path: "/Objetivos",
+  },
+  {
+    path: "/Hipotesis",
+  },
+  {
+    path: "/Propuesta",
+  },
+  {
+    path: "/Galeria",
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
