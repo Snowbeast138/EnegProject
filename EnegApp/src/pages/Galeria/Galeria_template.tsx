f193ba38b677adec9f9c4d29f6f0b722e343a239
Snowbeast138 <snowbeast138@gmail.com>
2024-06-18 17:02:21 -0600

    Galeria Complete

--- EnegApp/src/pages/Galeria/Galeria_template.tsx
+++ EnegApp/src/pages/Galeria/Galeria_template.tsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Title from "../../components/Title";
+import { Galleria } from "primereact/galleria";
+import { PhotoService } from "./PhotoService";
 
 function Galeria_template() {
+  const [images, setImages] = useState(null);
+
+  const responsiveOptions = [
+    {
+      breakpoint: "1024px",
+      numVisible: 5,
+    },
+    {
+      breakpoint: "960px",
+      numVisible: 4,
+    },
+    {
+      breakpoint: "768px",
+      numVisible: 3,
+    },
+    {
+      breakpoint: "560px",
+      numVisible: 1,
+    },
+  ];
+
+  useEffect(() => {
+    PhotoService.getImages().then((data) => setImages(data));
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const itemTemplate = (item) => {
+    return (
+      <img
+        src={item.itemImageSrc}
+        alt={item.alt}
+        style={{ width: "50%", display: "block" }}
+      />
+    );
+  };
+
+  const thumbnailTemplate = (item) => {
+    return (
+      <img
+        src={item.thumbnailImageSrc}
+        alt={item.alt}
+        style={{ display: "block" }}
+      />
+    );
+  };
+
   return (
     <div>
-      <Title content="Galería" />
+      <style>
+        {`
+          .galleria-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 50vh;
+            text-align: center;
+            padding: 20px;
+            box-sizing: border-box;
+          }
+
+          .galleria-container .p-galleria {
+            max-width: 50%;
+          }
+        `}
+      </style>
+      <Title content="Galeria" />
+      <div className="galleria-container">
+        <Galleria
+          value={images}
+          responsiveOptions={responsiveOptions}
+          numVisible={7}
+          circular
+          style={{ maxWidth: "800px" }}
+          item={itemTemplate}
+          showItemNavigators
+          showItemNavigatorsOnHover
+          showIndicators
+          showThumbnails={false}
+          autoPlay
+          transitionInterval={2000}
+        />
+      </div>
     </div>
   );
 }
---
+++ EnegApp/src/pages/Galeria/PhotoService.tsx
@@ -0,0 +1,136 @@
+export const PhotoService = {
+  getData() {
+    return [
+      {
+        itemImageSrc: "https://i.ibb.co/QCTbQpS/IMG-20240617-WA0093.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/QCTbQpS/IMG-20240617-WA0093.jpg",
+        alt: "Description for Image 1",
+        title: "Title 1",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/WyZcqK3/IMG-20240617-WA0092.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/WyZcqK3/IMG-20240617-WA0092.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/K03S8X3/IMG-20240617-WA0091.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/K03S8X3/IMG-20240617-WA0091.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/6sZBZdf/IMG-20240617-WA0090.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/6sZBZdf/IMG-20240617-WA0090.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/nbs3mWM/IMG-20240617-WA0089.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/nbs3mWM/IMG-20240617-WA0089.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/9rxpNJp/IMG-20240617-WA0088.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/9rxpNJp/IMG-20240617-WA0088.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/mSQWwqn/IMG-20240617-WA0087.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/mSQWwqn/IMG-20240617-WA0087.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/yF6yWR9/IMG-20240617-WA0086.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/yF6yWR9/IMG-20240617-WA0086.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/tpKZv8m/IMG-20240617-WA0085.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/tpKZv8m/IMG-20240617-WA0085.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/Cn5JybH/IMG-20240617-WA0084.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/Cn5JybH/IMG-20240617-WA0084.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/K9G9FRj/IMG-20240617-WA0083.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/K9G9FRj/IMG-20240617-WA0083.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/ZXyqmtq/IMG-20240617-WA0082.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/ZXyqmtq/IMG-20240617-WA0082.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/99PHBrm/IMG-20240617-WA0081.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/99PHBrm/IMG-20240617-WA0081.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/YNv2vTS/IMG-20240617-WA0080.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/YNv2vTS/IMG-20240617-WA0080.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/tpCkhdn/IMG-20240617-WA0079.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/tpCkhdn/IMG-20240617-WA0079.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/tPsfRMN/IMG-20240617-WA0078.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/tPsfRMN/IMG-20240617-WA0078.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/XVvvstZ/IMG-20240617-WA0077.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/XVvvstZ/IMG-20240617-WA0077.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/mq3Wxtd/IMG-20240617-WA0076.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/mq3Wxtd/IMG-20240617-WA0076.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/gD13jky/IMG-20240617-WA0075.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/gD13jky/IMG-20240617-WA0075.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/2nJTmWZ/IMG-20240617-WA0074.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/2nJTmWZ/IMG-20240617-WA0074.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+      {
+        itemImageSrc: "https://i.ibb.co/YW70J4x/IMG-20240617-WA0073.jpg",
+        thumbnailImageSrc: "https://i.ibb.co/YW70J4x/IMG-20240617-WA0073.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2",
+      },
+    ];
+  },
+
+  getImages() {
+    return Promise.resolve(this.getData());
+  },
+};
