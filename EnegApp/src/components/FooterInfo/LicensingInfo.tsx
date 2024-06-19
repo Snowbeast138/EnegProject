3e6698275a82b426ade08551712eefa3f260dc6b
Snowbeast138 <snowbeast138@gmail.com>
2024-06-18 20:36:35 -0600

    Bold Referencias

--- EnegApp/src/components/FooterInfo/LicensingInfo.tsx
+++ EnegApp/src/components/FooterInfo/LicensingInfo.tsx
@@ -15,29 +15,29 @@ function LicensingInfo() {
         <li style={{ fontWeight: "bold" }}>
           Kvale, 2008, Las entrevistas en investigación cualitativa. p. 37-39
         </li>
-        <li>
+        <li style={{ fontWeight: "bold" }}>
           Suárez, Ochoa, Dávila, s.f, MANUAL DE CAPACITACIÓN SOBRE REGISTRO Y
           SISTEMATIZACIÓN DE EXPERIENCIAS PEDAGÓGICA La documentación narrativa
           de experiencias escolares. p. 23
         </li>
-        <li>
+        <li style={{ fontWeight: "bold" }}>
           Red de Malnutrición en Iberoamérica del Programa de Ciencia y
           Tecnología para el Desarrollo. (2008). Análisis de determinantes
           sociales de la desnutrición en Latinoamérica. Nutrición Hospitalaria,
           25. https://scielo.isciii.es/scielo.php?pid=S0212-16112010000900003
           script=sci_arttext
         </li>
-        <li>
+        <li style={{ fontWeight: "bold" }}>
           La mala alimentación está perjudicando la salud de la infancia
           mundialmente. (s.
           f.).https://www.unicef.org/mexico/comunicados-prensa/la-mala-alimentaci%C3%B3n-est%C3%A1-perjudicando-la-salud-de-la-infancia-mundialmente
         </li>
-        <li>
+        <li style={{ fontWeight: "bold" }}>
           La mala alimentación perjudica la salud de los niños en todo el mundo,
           advierte UNICEF. (s. f.).
           https://www.unicef.org/es/comunicados-prensa/la-mala-alimentaci%C3%B3n-perjudica-la-salud-de-los-ni%C3%B1os-en-todo-el-mundo-advierte
         </li>
-        <li>
+        <li style={{ fontWeight: "bold" }}>
           Efecto de la mala nutrición en los primeros años de vida. (2020, 23
           noviembre). Alimentación y Salud.
           https://alimentacionysalud.unam.mx/efecto-de-la-mala-nutricion-en-los-primeros-anos-de-vida/
